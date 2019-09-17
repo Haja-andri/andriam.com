239af41a794992e0047c94964cd52df4bb03a6d2
Haja Andriamaro <haja.andri@gmail.com>
2019-09-17 16:28:40 +0200

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
+    title: `Haja Andriamaro Blog and Learnings`,
+    name: `Haja Andriamaro`,
     siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    description: `On this blog I share my struggles and breakthrough as Fullstack Web developper `,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `There is no real learning wihtout pains.`,
       maxWidth: 652,
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/haja_andriamaro`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://github.com/Haja-andri`,
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://www.linkedin.com/in/haja-andriamaro-b145412/`,
       },
     ],
   },
