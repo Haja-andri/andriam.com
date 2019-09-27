a092add584f1f3a1a76392d9111aeafb73a3989c
Haja Andriamaro <haja.andri@gmail.com>
2019-09-27 17:19:47 +0200

    fix tentative

--- src/@narative/gatsby-theme-novela/components/MDX/MDX.tsx
+++ src/@narative/gatsby-theme-novela/components/MDX/MDX.tsx
@@ -189,6 +189,7 @@ const PrismCSS = p => css`
 
     ${mediaqueries.phablet`
       font-size: 1.3rem;
+      text-size-adjust: none;
       border-radius: 0px;
       margin: 0 auto 25px;
       padding: 15px 10px;
