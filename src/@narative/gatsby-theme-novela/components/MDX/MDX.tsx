4ff8ef06f42a7a25c6130e0a6d589ad3d0190bc7
Haja Andriamaro <haja.andri@gmail.com>
2019-09-27 17:42:43 +0200

    removed the border to make it the scroll less visible on the code container

--- src/@narative/gatsby-theme-novela/components/MDX/MDX.tsx
+++ src/@narative/gatsby-theme-novela/components/MDX/MDX.tsx
@@ -190,7 +190,6 @@ const PrismCSS = p => css`
     ${mediaqueries.phablet`
       font-size: 1.3rem;
       text-size-adjust: none;
-      border:1px solid grey;
       border-radius: 5px;
       margin: 0 auto 25px;
       padding: 15px 10px;
